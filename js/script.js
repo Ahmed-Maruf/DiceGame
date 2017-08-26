d9bd6637f539578c17768fd3176c2e3fffbb2048
Ahmed-Maruf <acmicpc04@gmail.com>
2017-08-27 01:17:28 +0600

    simple game using js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*
+
+
+var computer = {
+	model:"2016",
+	ram:'8GB',
+	quantity:1,
+	type:"laptop",
+	fullType: function(){
+		return this.type;
+	}
+};
+
+document.write(computer.fullType());
+
+
+var person = new Object();
+*/
+
+
+$(document).ready(function () {
+	$('h1').fadeOut();
+});
---
+++ js/solving.js
@@ -0,0 +1,3 @@
+var x = readline();
+
+x==2 || x%2==1? print('NO'):print('YES');
